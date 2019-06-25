d9eb3cf704ad68413e889b449b55821d15a39a55
Kenneth McClure <kenneth.98@live.co.uk>
2019-06-25 15:47:23 +0100

    Minor tweak

--- BasicLED/two_leds.py
+++ BasicLED/two_leds.py
@@ -8,23 +8,29 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(13, GPIO.OUT)
 GPIO.setup(5, GPIO.OUT)
 
-# Set the number of flashes:
-numb_flashes = 10
+try:
+    # Set the number of flashes:
+    numb_flashes = 10
 
-# Set the duration of flashes (s):
-flash_duration = 0.5
+    # Set the duration of flashes (s):
+    flash_duration = 0.5
 
-for i in range(numb_flashes):
-    
-    GPIO.output(13, True)
-    GPIO.output(5, False)
+    for i in range(numb_flashes):
+        
+        GPIO.output(13, True)
+        GPIO.output(5, False)
 
-    time.sleep(flash_duration)
-    
-    GPIO.output(13, False)
-    GPIO.output(5, True)
-    
-    time.sleep(flash_duration)
-
-# Clean up all the ports that have been used:
-GPIO.cleanup()
+        time.sleep(flash_duration)
+        
+        GPIO.output(13, False)
+        GPIO.output(5, True)
+        
+        time.sleep(flash_duration)
+except KeyboardInterrupt:
+    print("KeyboardInterrupt, exiting...")
+except:
+    # other errors...
+    print("Some other error has occurred.")
+finally:
+    # Clean up all the ports that have been used:
+    GPIO.cleanup()
